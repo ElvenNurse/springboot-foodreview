213853b154b72eceb8d72a0f8e3f348d902048e3
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2020-03-01 19:17:18 +0200

    Created SpringBoot Project

---
+++ src/test/java/mate/academy/boot/amazonreview/AmazonReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.boot.amazonreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
