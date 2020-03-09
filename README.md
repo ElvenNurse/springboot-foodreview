5af8d3e8d9b8a0291d2f37cdb31393fe18a3fc32
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2020-03-09 16:28:03 +0200

    Added JWT support

--- README.md
+++ README.md
@@ -22,22 +22,51 @@ Run application, on first run it will download file with data (~300MB).<br>
 Wait until data from file will be inserted into DataBase (DB).<br><br>
 After receiving message like<br>
 <b>Started AmazonReviewApplication in 60.295 seconds (JVM running for 61.352)</b><br>
-open following link in your web browser: 
-<a href="http://localhost:8080/">http://localhost:8080/</a><br>
-You will see the login page, use <b>user</b> as username and <b>1234</b> as password<br>
+application is running and you can continue work with it.
 
-To get most active users open following link:
-<a href="http://localhost:8080/reviews/top-users">Top users</a>
+### Authorization
 
-To get most commented food items open following link:
-<a href="http://localhost:8080/reviews/top-products">Top products</a>
+Application using JWT token for authentication.
+You need to get JWT token by sending **POST** request to:<br>
+<a href="http://localhost:8080/authenticate">http://localhost:8080/authenticate</a><br>
+with body 
+<pre>
+{
+    "username":"user", 
+    "password":"1234"
+}
+</pre>
+From response you need get token (example of response):
+<pre>
+{
+    "jwt": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNTgzNzgwMDE1LCJpYXQiOjE1ODM3NjIwMTV9.E2Hc-Kdytev220OG83Fgpu3PF7Y11eV036lOpeq6JvlJx2l0QufpcGFKZijpNpo1kWAyTcb3KWrqKPEC-m6M_g"
+}
+</pre>
+and add Authorization header for you next requests with **Bearer** + token, for example:<br>
+<pre>
+Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNTgzNzgwMDE1LCJpYXQiOjE1ODM3NjIwMTV9.E2Hc-Kdytev220OG83Fgpu3PF7Y11eV036lOpeq6JvlJx2l0QufpcGFKZijpNpo1kWAyTcb3KWrqKPEC-m6M_g
+</pre>
 
-To get most used words open following link:
-<a href="http://localhost:8080/reviews/top-words">Top words</a>
+### Review controller
+
+There are following functions available:
+
+To get most active users use following link:
+<a href="http://localhost:8080/reviews/top-users">localhost:8080/reviews/top-users</a>
+
+To get most commented food items use following link:
+<a href="http://localhost:8080/reviews/top-products">localhost:8080/reviews/top-products</a>
+
+To get most used words use following link:
+<a href="http://localhost:8080/reviews/top-words">localhost:8080/reviews/top-words</a>
+
+Use **GET** method, also pagination available.
+Use parameters "**page**" and "**limit**", for example:<br>
+<a href="http://localhost:8080/reviews/top-users?page=3&limit=10">localhost:8080/reviews/top-users?page=3&limit=10</a>
 
 ## Technology stack
 
-Java 11, Spring Boot, Spring WebMVC, Spring Data, Spring Security, Swagger, H2 DB, Jackson CSV
+Java 11, Spring Boot, Spring WebMVC, Spring Data, Spring Security, JWT, Swagger, H2 DB, Jackson CSV
 
 ## Author
 
---
+++ src/main/java/mate/academy/boot/amazonreview/filter/JwtRequestFilter.java
@@ -0,0 +1,58 @@
+package mate.academy.boot.amazonreview.filter;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.academy.boot.amazonreview.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+    private UserDetailsService userDetailsService;
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    public JwtRequestFilter(UserDetailsService userDetailsService, JwtUtil jwtUtil) {
+        this.userDetailsService = userDetailsService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        final String authorizationHeader = httpServletRequest.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            username = jwtUtil.getUsernameFromToken(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null,
+                                userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource()
+                        .buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
